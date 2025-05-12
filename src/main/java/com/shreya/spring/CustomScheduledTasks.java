5377eaf4254ff9a08beb35c2aa21979954a73328
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-12 13:23:18 +0530

    updated

---
+++ src/main/java/com/shreya/spring/CustomScheduledTasks.java
@@ -0,0 +1,31 @@
+package com.shreya.spring;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.time.LocalDateTime;
+import java.util.concurrent.Executor;
+
+@Configuration
+public class CustomScheduledTasks implements SchedulingConfigurer {
+
+	@Autowired
+	private Executor poolScheduler;
+
+	@Scheduled(fixedRate = 5000)
+	public void scheduledTask() {
+		System.out.println("Task executed at " + LocalDateTime.now() + " on thread " + Thread.currentThread().getName());
+	}
+
+	@Scheduled(fixedDelayString = "${fixedDelay.in.milliseconds}")
+	public void scheduleTaskWithFixedDelay() {
+		System.out.println("Fixed delay task executed at " + LocalDateTime.now() + " on thread " + Thread.currentThread().getName());
+	}
+
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+		taskRegistrar.setScheduler(poolScheduler);
+	}
+}
--- src/main/java/com/shreya/spring/config/SchedulerConfig.java
+++ src/main/java/com/shreya/spring/config/SchedulerConfig.java
@@ -1,4 +1,29 @@
 package com.shreya.spring.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
-public class SchedulerConfig {
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig implements SchedulingConfigurer {
+
+    @Bean
+    public Executor taskExecutor() {
+        return Executors.newScheduledThreadPool(10);
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(taskExecutor());
+    }
+
+    @Bean(name = "poolScheduler")
+    public Executor poolScheduler() {
+        return Executors.newScheduledThreadPool(5);
+    }
 }
